d99f8bb95e97937d4f10339a7437f9547406de4e
Wessel <iwesseltwitch@gmail.com>
2023-10-03 14:01:05 +0200

    Connectie met de AI Api gemaakt

---
+++ BusinessLogic/CharacterCollection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+	public class CharacterCollection
+	{
+		public void Test()
+		{
+
+		}
+
+		public async Task<string> CreateCharacterAsync(string input)
+		{
+			string apiUrl = "https://api-d7b62b.stack.tryrelevance.com/latest/studios/cafafd66-ee8c-4764-ba15-65fcd86ec9b7/trigger_limited";
+
+			string jsonBody = $@"{{
+				""params"": {{
+					""long_text"": ""{input}""
+				}},
+				""project"": ""6f2b3a705849-4ac5-b8df-d50bd22fde95""
+			}}";
+
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					StringContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+					HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+					if (response.IsSuccessStatusCode)
+					{
+						string responseBody = await response.Content.ReadAsStringAsync();
+
+						return responseBody;
+					}
+					else
+					{
+						return $"Fout: Statuscode {response.StatusCode}";
+					}
+				}
+				catch (Exception ex)
+				{
+					return $"Fout: {ex.Message}";
+				}
+			}
+		}
+
+		private string GetAnswerFromJson(string json)
+		{
+			try
+			{
+				using (JsonDocument doc = JsonDocument.Parse(json))
+				{
+					JsonElement root = doc.RootElement;
+					if (root.TryGetProperty("output", out JsonElement outputElement) &&
+						outputElement.TryGetProperty("answer", out JsonElement answerElement))
+					{
+						string answer = answerElement.GetString();
+						return GetAnswerFromJson(answer);
+					}
+					else
+					{
+						return "JSON structure is not as expected.";
+					}
+				}
+			}
+			catch (JsonException ex)
+			{
+				return $"JSON parsing error: {ex.Message}";
+			}
+		}
+	}
+}
---
+++ WebApi/Controllers/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CharacterController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<IActionResult> GetCharacter([FromQuery] string input)
+		{
+			try
+			{
+				CharacterCollection charCollection = new CharacterCollection();
+
+				// Wacht op de resultaten van de asynchrone methode.
+				var result = await charCollection.CreateCharacterAsync(input);
+
+				// Retourneer de respons nadat de gegevens beschikbaar zijn.
+				return Ok(result);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
