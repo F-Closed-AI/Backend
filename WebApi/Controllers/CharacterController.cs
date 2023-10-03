549b1611a683f8b3e07097e18c73bf8680900c30
Wessel <iwesseltwitch@gmail.com>
2023-10-03 15:30:51 +0200

    Frontend URL aangepast.

--- WebApi/Controllers/CharacterController.cs
+++ WebApi/Controllers/CharacterController.cs
@@ -1,8 +1,6 @@
 ﻿using BusinessLogic;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace WebApi.Controllers
 {
 	[Route("api/[controller]")]
@@ -16,10 +14,8 @@ namespace WebApi.Controllers
 			{
 				CharacterCollection charCollection = new CharacterCollection();
 
-				// Wacht op de resultaten van de asynchrone methode.
 				var result = await charCollection.CreateCharacterAsync(input);
 
-				// Retourneer de respons nadat de gegevens beschikbaar zijn.
 				return Ok(result);
 			}
 			catch (Exception ex)
