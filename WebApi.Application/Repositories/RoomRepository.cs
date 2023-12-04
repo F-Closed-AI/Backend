4b06115e007337f3f318160279d830f6f2baca3b
Vincent Scheffer <vincentscheffer2@gmail.com>
2023-12-04 12:15:19 +0100

    hopeloos

---
+++ WebApi.Application/Repositories/RoomRepository.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Interfaces;
+using WebApi.Application.Models;
+
+namespace WebApi.Application.Repositories
+{
+    public class RoomRepository
+    {
+        private IMongoCollection<Room> _room;
+
+        public RoomRepository(IDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _room = database.GetCollection<Room>(settings.CollectionName);
+        }
+
+        public async Task<Room> CreateRoom(Room room)
+        {
+            await _room.InsertOneAsync(room);
+            return room;
+        }
+    }
+}
---
+++ WebApi.Application/Services/RoomCollectionService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Models;
+using WebApi.Application.Repositories;
+
+namespace WebApi.Application.Services
+{
+    public class RoomCollectionService
+    {
+
+        private readonly RoomRepository _roomRepository;
+        public RoomCollectionService(RoomRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+
+
+        public async Task<Room> CreateRoom(Room room)
+        {
+            return await _roomRepository.CreateRoom(room);
+        }
+    }
+}
