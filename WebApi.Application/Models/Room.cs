a9d5d8b80fea169e82154f16985b23e222d31046
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:47:46 +0100

    Updated the room entity

--- WebApi.Application/Models/Room.cs
+++ WebApi.Application/Models/Room.cs
@@ -1,11 +1,6 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebApi.Application.Models
 {
@@ -14,10 +9,14 @@ namespace WebApi.Application.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? RoomId { get; set; } = ObjectId.GenerateNewId().ToString();
+        [Required]
+        public string Name { get; set; }
         [Required]
         public int UserId { get; set; }
-
         public List<string>? CharId { get; set; }
         public bool IsDeleted { get; set; } = false;
+        public DateTime DateTime { get; set; } = DateTime.UtcNow;
     }
 }
