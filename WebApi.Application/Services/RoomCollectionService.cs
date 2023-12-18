99c97da3f14491b8317cbea2fb27110f1612b473
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:48:46 +0100

    Updated the room collection service

--- WebApi.Application/Services/RoomCollectionService.cs
+++ WebApi.Application/Services/RoomCollectionService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebApi.Application.Models;
+﻿using WebApi.Application.Models;
 using WebApi.Application.Repositories;
 
 namespace WebApi.Application.Services
