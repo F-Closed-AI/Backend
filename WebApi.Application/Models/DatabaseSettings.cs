d2eff463ee9601571a689a4a5c36f3d113fe499d
2ndZorua <485892@student.fontys.nl>
2023-12-18 10:48:02 +0100

    Updated the database settings entity

--- WebApi.Application/Models/DatabaseSettings.cs
+++ WebApi.Application/Models/DatabaseSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebApi.Application.Interfaces;
+﻿using WebApi.Application.Interfaces;
 
 namespace WebApi.Application.Models
 {
